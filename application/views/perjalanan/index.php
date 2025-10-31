2c377a099fbc82453c8e3482154e4d8bcd5a09d2
Henoch <henochyanuar13@gmail.com>
2025-10-31 22:03:25 +0700

    refactor(perjalanan): change the index perjalanan file name and the directory

--- application/views/perjalanan/index.php
+++ application/views/perjalanan/index.php
@@ -1,4 +1,8 @@
  <div class="card">
+    <div class="card-header">
+         <a href="<?= site_url('perjalanan/tambah') ?>" class="btn btn-primary btn-sm">Tambah Data</a>
+     </div>
+     <!-- /.card-header -->
      <div class="card-body">
          <table id="example1" class="table table-bordered table-striped">
              <thead>
