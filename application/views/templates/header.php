cd02ff301ac22a7b27dfef7400b181327670d6fe
Henoch <henochyanuar13@gmail.com>
2025-11-04 11:01:37 +0700

    refactor(template): change the logo and icon app

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -21,5 +21,7 @@
     
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-  
+
+  <link rel="icon" href="<?= base_url('assets/template') ?>/dist/img/logo_rsfc.png" type="image/x-icon">
+
 </head>
--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -35,7 +35,7 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="#" class="brand-link">
-                <img src="<?= base_url('assets/template/') ?>dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
+                <img src="<?= base_url('assets/template/') ?>dist/img/logo_rsfc.png" alt="AdminLTE Logo"
                     class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">SIDARMINI</span>
             </a>
